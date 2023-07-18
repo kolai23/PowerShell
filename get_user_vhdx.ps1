9e888920e2df7eff1289ec3f63ee16620c7583e0
kolai23 <q3181309@gmail.com>
2023-07-18 14:57:03 +0300

    Update get_user_vhdx.ps1

--- get_user_vhdx.ps1
+++ get_user_vhdx.ps1
@@ -1,10 +1,13 @@
-﻿$usersn = 'Borodich'
+﻿$usersn = 'xxxxx'
 $users = Get-ADUser  -filter 'SamAccountName -eq $usersn' -Properties dateOfDism,lastLogonDate,sid,SamAccountName  
 $path_ltfs02 = '\\ltfs02\D$\user_profiles\'
 $path_ltfs03 = '\\ltfs03\D$\User_profiles$\'
 foreach ($user in $users) {
 
-   $user_sid =[string]::Format('UVHD-{0}.vhdx',$user.SID)
+   $user_sid1 = Get-ChildItem -Recurse -Path $path_ltfs02 -Include $user_sid
+   $user_sid2 = Get-ChildItem -Recurse -Path $path_ltfs03 -Include $user_sid
+
+   write-host ($user_sid1 +'             '+ $user_sid2)
 
    write-host ($user_sid)
 }
